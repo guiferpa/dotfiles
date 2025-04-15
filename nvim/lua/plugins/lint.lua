1138cb4b4e04f1062f17a3848ea037df0e02f505
Guilherme Paixão <guiferpa@gmail.com>
2025-04-15 10:25:42 -0300

    chore: configured linters

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,7 +5,13 @@ return {
     config = function()
       local lint = require("lint")
 
-      lint.linters_by_ft = {}
+      lint.linters_by_ft = {
+        python = { "ruff" },
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" }
+      }
 
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
