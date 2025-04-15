1e1e8ed048e70685baae7dd0001675b06e8287e6
Guilherme Paixão <guiferpa@gmail.com>
2025-04-15 16:30:41 -0300

    chore: added python as a dep in treesitter plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "rust", "go", "clojure", "typescript", "tsx", "javascript", "html" },
+        ensure_installed = { "c", "lua", "vim", "rust", "go", "clojure", "typescript", "tsx", "javascript", "html", "python" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
